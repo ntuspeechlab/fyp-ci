1adad7b5617c202b558a85d94ff1cb1760f16e89
kaikiat <pohkaikiat98@gmail.com>
2022-09-14 00:44:21 +0800

    update pipeline

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-# #!/usr/bin/env python3
-# from flask import Flask
-# app = Flask(__name__)
+#!/usr/bin/env python3
+from flask import Flask
+app = Flask(__name__)
 
-# #dasd
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, Docker! Version 0.0.3'
+#dasd
+@app.route('/')
+def hello_world():
+    return 'Hello, Docker! Version 0.0.3'
