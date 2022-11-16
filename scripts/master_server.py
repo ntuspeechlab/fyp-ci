8c18bd6c7e1e36a96599010ed08900acb584713c
kaikiat <pohkaikiat98@gmail.com>
2022-11-16 23:29:32 +0800

    update workflows

--- scripts/master_server.py
+++ scripts/master_server.py
@@ -121,7 +121,7 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         now = datetime.now()
         self.set_status(200)
-        self.finish("Speechlab Streamer" + str(now) + 'abc')
+        self.finish("Speechlab Streamer" + str(now) + 'abcde')
 
 class TestConnectionHandler(tornado.web.RequestHandler):
     def get(self):
